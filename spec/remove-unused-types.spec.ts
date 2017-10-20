1e471bc134ecd991f4e13fef76fa816bbb986989
Jan Melcher <jan.melcher@aeb.com>
2017-10-20 23:59:06 +0200

    Simplify specs using graphql-tag

--- spec/remove-unused-types.spec.ts
+++ spec/remove-unused-types.spec.ts
@@ -1,78 +1,45 @@
-import {
-    buildASTSchema, GraphQLID, GraphQLInterfaceType, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString, parse
-} from 'graphql';
+import { buildASTSchema } from 'graphql';
 import { removeUnusedTypesFromSchema } from '../src/remove-unused-types';
 import gql from 'graphql-tag';
 
 describe('remove-unused-types', () => {
     it('removes unused types', () => {
-        const iface = new GraphQLInterfaceType({
-            name: 'Interface',
-            fields: {
-                test: {
-                    type: GraphQLID
-                }
-            },
-            resolveType: () => ''
-        });
+        const schema = buildASTSchema(gql`
+            schema {
+                query: Query
+            }
+            interface Interface {
+                test: ID
+            }
+            type Query {
+                hello: String
+            }
+            type Impl implements Interface {
+                test: ID
+            }
+        `);
 
-        const schema = new GraphQLSchema({
-            query: new GraphQLObjectType({
-                name: 'Query',
-                fields: {
-                    hello: {
-                        type: GraphQLString
-                    }
-                }
-            }),
-            types: [new GraphQLObjectType({
-                name: 'Unused',
-                interfaces: [ iface ],
-                fields: {
-                    test: {
-                        type: GraphQLID
-                    }
-                }
-            })]
-        });
-
-        expect(schema.getTypeMap()['Unused']).toBeDefined(); // sanity check that GraphQL does not remove this
+        expect(schema.getTypeMap()['Impl']).toBeDefined(); // sanity check that GraphQL does not remove this
         const condensedSchema = removeUnusedTypesFromSchema(schema);
-        expect(condensedSchema.getTypeMap()['Unused']).toBeUndefined();
+        expect(condensedSchema.getTypeMap()['Impl']).toBeUndefined();
     });
 
     it('keeps interface implementations if they are still in use', () => {
-        const iface = new GraphQLInterfaceType({
-            name: 'Interface',
-            fields: {
-                test: {
-                    type: GraphQLID
-                }
-            },
-            resolveType: () => ''
-        });
+        const schema = buildASTSchema(gql`
+            schema {
+                query: Query
+            }
+            interface Interface {
+                test: ID
+            }
+            type Query {
+                hello: Interface
+            }
+            type Impl implements Interface {
+                test: ID
+            }
+        `);
 
-        const schema = new GraphQLSchema({
-            query: new GraphQLObjectType({
-                name: 'Query',
-                fields: {
-                    hello: {
-                        type: iface
-                    }
-                }
-            }),
-            types: [new GraphQLObjectType({
-                name: 'Impl',
-                interfaces: [ iface ],
-                fields: {
-                    test: {
-                        type: GraphQLID
-                    }
-                }
-            })]
-        });
-
-        expect(schema.getTypeMap()['Impl']).toBeDefined(); // sanity check that GraphQL does not remove this
         const condensedSchema = removeUnusedTypesFromSchema(schema);
         expect(condensedSchema.getTypeMap()['Impl']).toBeDefined();
     });
@@ -118,102 +85,51 @@ describe('remove-unused-types', () => {
     });
 
     it('keeps interface implementations if they are still used indirectly', () => {
-        const iface = new GraphQLInterfaceType({
-            name: 'Interface',
-            fields: {
-                test: {
-                    type: GraphQLID
-                }
-            },
-            resolveType: () => ''
-        });
-        const iface2 = new GraphQLInterfaceType({
-            name: 'Interface2',
-            fields: {
-                test: {
-                    type: iface
-                }
-            },
-            resolveType: () => ''
-        });
+        const schema = buildASTSchema(gql`
+            schema {
+                query: Query
+            }
+            interface Interface {
+                test: ID
+            }
+            interface WrappingInterface {
+                test: Interface
+            }
+            type Query {
+                hello: WrappingInterface
+            }
+            type Impl implements Interface {
+                test: ID
+            }
+        `);
 
-        const schema = new GraphQLSchema({
-            query: new GraphQLObjectType({
-                name: 'Query',
-                fields: {
-                    hello: {
-                        type: iface2
-                    }
-                }
-            }),
-            types: [new GraphQLObjectType({
-                name: 'Impl',
-                interfaces: [ iface ],
-                fields: {
-                    test: {
-                        type: GraphQLID
-                    }
-                }
-            })]
-        });
-
-        expect(schema.getTypeMap()['Impl']).toBeDefined(); // sanity check that GraphQL does not remove this
         const condensedSchema = removeUnusedTypesFromSchema(schema);
         expect(condensedSchema.getTypeMap()['Impl']).toBeDefined();
     });
 
     it('keeps interface implementations if they are still used indirectly through impl fields', () => {
-        const iface = new GraphQLInterfaceType({
-            name: 'Interface',
-            fields: {
-                test: {
-                    type: GraphQLID
-                }
-            },
-            resolveType: () => ''
-        });
-        const iface2 = new GraphQLInterfaceType({
-            name: 'Interface2',
-            fields: {
-                test: {
-                    type: GraphQLID
-                }
-            },
-            resolveType: () => ''
-        });
+        const schema = buildASTSchema(gql`
+            schema {
+                query: Query
+            }
+            interface Interface1 {
+                test: ID
+            }
+            interface Interface2 {
+                test: ID
+            }
+            type Query {
+                hello: Interface1
+            }
+            type Impl1 implements Interface1 {
+                test: ID
+                otherField: Interface2
+            }
+            type Impl2 implements Interface2 {
+                test: ID
+            }
+        `);
 
-        const schema = new GraphQLSchema({
-            query: new GraphQLObjectType({
-                name: 'Query',
-                fields: {
-                    hello: {
-                        type: iface2
-                    }
-                }
-            }),
-            types: [new GraphQLObjectType({
-                name: 'Impl',
-                interfaces: [ iface ],
-                fields: {
-                    test: {
-                        type: GraphQLID
-                    },
-                    otherFields: {
-                        type: iface2
-                    }
-                }
-            }), new GraphQLObjectType({
-                name: 'Impl2',
-                interfaces: [ iface2 ],
-                fields: {
-                    test: {
-                        type: GraphQLID
-                    }
-                }
-            })]
-        });
-
-        expect(schema.getTypeMap()['Impl2']).toBeDefined(); // sanity check that GraphQL does not remove this
         const condensedSchema = removeUnusedTypesFromSchema(schema);
         expect(condensedSchema.getTypeMap()['Impl2']).toBeDefined();
     });
